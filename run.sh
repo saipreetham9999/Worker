d035716af23f913b5f7ca1ae21dfe541f47e54dd
saipreetham9999 <saipreethamreddy8@gmail.com>
2026-02-14 01:30:26 -0600

    test phptp

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 REPO_URL="https://github.com/saipreetham9999/Worker.git"
 DIR_NAME="Worker"
 
+git fetch origin
+git reset --hard origin/main
+
 
 # Install dependencies quietly
     echo "📦 Installing dependencies..."
@@ -9,3 +12,4 @@ DIR_NAME="Worker"
 
 echo "✅ STARTING WORKER..."
 python3 main.py
+
